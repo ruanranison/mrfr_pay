a40091fb690a51851f2921d002287abdace953d6
Marquin <jmcs6@aluno.ifal.edu.br>
2023-01-10 15:44:20 -0300

    add addressapi

---
+++ lib/data/address_api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:mrfr_pay/domain/address.dart';
+
+class AddressApi {
+  findAddressByCep(String cep) async {
+    Uri url = Uri.http("viacep.com.br", "/ws/$cep/json/");
+
+    Response response = await http.get(url);
+    Map<String, dynamic> json = jsonDecode(response.body);
+
+    print(response.statusCode);
+    print(response.body);
+
+    // Status code != 200
+    if (json["erro"] != null) {
+      return Address();
+    }
+
+    // Status code == 200
+    Address address = Address.fromJson(json);
+    return address;
+  }
+}
